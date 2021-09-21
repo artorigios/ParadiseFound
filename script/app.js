9efb27d0821de5f5607b9f896bd21674635ff42d
Artus Geisler <tristr4m@vivaldi.net>
2021-09-21 23:51:06 +0200

    sidenav

--- script/app.js
+++
@@ -1,7 +0,0 @@
-const navSlide = document.querySelector('.slide');
-const page = document.querySelector('.page');
-
-navSlide.addEventListener('click', () => {
-    navSlide.classList.toggle('active');
-    page.classList.toggle('active');
-})
